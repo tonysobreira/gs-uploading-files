ffde4a9929f84fddc725a7ed633c6f91fb004191
Tony <tonysobreira@gmail.com>
2020-11-05 11:10:37 -0300

    commit

---
+++ src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.uploadingfiles.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+	private static final long serialVersionUID = 1L;
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
